58f8081af6e6341a48458330e13d3621c0697bb9
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2026-09-13 21:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Flask CI/CD Demo 🚀</h1>
+    <h1>Flask CI/CD Demo by 🚀</h1>
     <p>Deployed using GitHub Actions + Docker + Helm + Argo CD + Kubernetes</p>
     """
 
