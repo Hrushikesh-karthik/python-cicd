02272a7cf1df707ebeb2613b2535557974fc5a43
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2026-09-13 21:12:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -46,4 +45,4 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
->>>>>>> 489f4fa (Initial commit)
+
